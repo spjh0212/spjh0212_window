823b6bb40cb0a74d9362d5d859ce9e61bcd3765f
spjh0212 <spjh0212@gmail.com>
2024-06-04 14:01:49 +0800

    modify

--- 2024_06_04/index.py
+++ 2024_06_04/index.py
@@ -1,8 +1,18 @@
 from pprint import pprint
+import tkinter as tk
+from tkinter import ttk
+from ttkthemes import ThemedTk
 import tools
 
 
+class Window(ThemedTk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.title('AQI顯示')
+        ttk.Button(self, text="Quit", command=self.destroy).pack()
+
 def main():
+    '''
     try:
         all_data:dict[any] = tools.download_json()
     except Exception as error:
@@ -10,6 +20,10 @@ def main():
     else:        
         data:list[dict] = tools.get_data(all_data)
         pprint(data)
+    '''
+
+    window = Window(theme="itft1")
+    window.mainloop()
 
 if __name__ == '__main__':
     main()
